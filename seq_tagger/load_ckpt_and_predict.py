3c27b8c5a7f1681858ae8a0323b07286caa47ef0
kyleclo <kyleclo@uw.edu>
2022-01-13 02:19:21 -0800

    add script to load and predict from ckpt

---
+++ seq_tagger/load_ckpt_and_predict.py
@@ -0,0 +1,84 @@
+"""
+
+Loads model checkpoint and runs predictions on a given dataset.
+
+
+Pretty hacky script but PTL doesn't have good documentation on how to use its models for prediction. This seems easiest.
+Just note.  The hackiest part of this is that we're pointing to an input directory containing train|val|test.jsonl
+but we're only using test.jsonl for predictions.
+
+
+
+Usage?
+
+For example, let's use this to get the predictions for every epoch on the test data (as opposed to just epoch 4):
+
+
+    for window in 1 3 5 7 9 11
+    do
+        for fold in 0 1 2 3 4
+        do
+            for epoch in 0 1 2 3
+            do
+              ckpt="/net/nfs2.corp/s2-research/kylel/multicite-2022/output/allenai-scibert_scivocab_uncased__${window}__1__07-01-02__batch32/${fold}/epoch=0${epoch}*.ckpt"
+              python load_ckpt_and_predict.py \
+              --input /net/nfs2.corp/s2-research/kylel/multicite-2022/input/allenai-scibert_scivocab_uncased__${window}__1__07-01-02/ \
+              --ckpt ${ckpt} \
+              --output /net/nfs2.corp/s2-research/kylel/multicite-2022/test_output/allenai-scibert_scivocab_uncased__${window}__1__07-01-02__batch32/${fold}/
+              --batch_size 32 \
+              --gpus 1 \
+              --use_intent
+            done
+        done
+    done
+
+
+"""
+
+import os
+import argparse
+import json
+
+from pytorch_lightning import Trainer
+
+try:
+    from seq_tagger.train_seq_tagger import MyTransformer, MyDataModule, MyDataset
+except ImportError:
+    from train_seq_tagger import MyTransformer, MyDataModule, MyDataset
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='Path to data directory.', required=True)
+    parser.add_argument('-c', '--ckpt', type=str, help='Path to model ckpt file.', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Path to directory for output.', required=True)
+    parser.add_argument('--model_name_or_path', type=str, default='allenai/scibert_scivocab_uncased')
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--gpus', type=int, default=0)
+    parser.add_argument('--use_intent', action='store_true')
+    args = parser.parse_args()
+
+    os.makedirs(args.output, exist_ok=True)
+
+    # load model
+    model = MyTransformer.load_from_checkpoint(checkpoint_path=args.ckpt)
+    model.val_pred_output_path = args.output
+    model.test_pred_output_path = args.output       # hacky, but it works :/
+
+    # load data using PTL Data module. Also not necessary, but keeps things short.
+    dm = MyDataModule(model_name_or_path=args.model_name_or_path,
+                      max_seq_length=512,
+                      batch_size=args.batch_size,
+                      cache_dir=args.input,
+                      use_intent=args.use_intent)
+    dm.setup()
+
+    # use PTL's trainer to avoid writing predition loop. also hacky, but keeps code short.
+    trainer = Trainer(gpus=args.gpus, progress_bar_refresh_rate=5, max_steps=0, limit_val_batches=0)
+
+    # do the prediction
+    results = trainer.test(model, dm.test_dataloader())
+    assert os.path.exists(os.path.join(args.output, 'test-0.jsonl'))
+
+
