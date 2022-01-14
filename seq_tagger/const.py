6ae059abc87efe110a4438992bfbd8a84854557b
kyleclo <kyleclo@uw.edu>
2022-01-13 21:17:35 -0800

    add consts

--- seq_tagger/const.py
+++ seq_tagger/const.py
@@ -6,3 +6,7 @@ IGNORE_SENTS = {'----------------------------------', '****'}
 
 
 PAD_TOKEN_ID = -100
+
+
+WRONG_INTENT_INSTANCE_START_ID = 30000      # note that the gold instances can go up to id=12571
+CLIPPED_INSTANCE_START_ID = 300000          # consider num_gold_instances X all intents can go up to id=100k
